4aea8d471308b2374424a72bb2396fa7070249fb
BASIL » <77526924+dmxbasil@users.noreply.github.com>
2024-06-28 01:28:42 +0530

    ᴀᴅ ꜰʀᴇᴇ ❗

--- info.py
+++ info.py
@@ -101,7 +101,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shrinkme.io')
 SHORTLINK_API = environ.get('SHORTLINK_API', '1187d8b7c3e869218765eeae775f838ed716aa98')
 
